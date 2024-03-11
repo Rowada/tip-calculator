b0fd6bcc7a14617433289e83d5388a86ba0b4606
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-11 13:29:07 +0100

    style: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { CalculatorContainer } from "./components/Calculator/CalculatorContainer";
+import { Header } from "./components/Header/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <CalculatorContainer />
     </>
   );
 }
---
+++ src/components/Calculator/CalculatorContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CalculatorContainer = () => {
+  return (
+    <div className="bg-containerBackground rounded-xl text-center py-2 px-2 w-[1200px] h-[600px] ">
+      CalculatorContainer
+    </div>
+  );
+};
