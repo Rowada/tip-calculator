d7943b326166773bc021c9b38d2b10c58e301371
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-11 10:40:11 +0100

    style: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(172, 67%, 45%)",
+        accent: "hsl(183, 100%, 15%)",
+        text: "hsl(186, 14%, 43%)",
+        text2: "hsl(184, 14%, 56%)",
+        background: "hsl(185, 41%, 84%)",
+        inputBackground: "hsl(189, 41%, 97%)",
+        containerBackground: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
