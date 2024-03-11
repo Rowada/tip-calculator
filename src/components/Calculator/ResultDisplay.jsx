e78e8cc381c09b10181145a932a7787a3cc8d5d5
Josue <96113041+Rowada@users.noreply.github.com>
2024-03-11 15:09:05 +0100

    style: add first version for result component

--- src/components/Calculator/ResultDisplay.jsx
+++ src/components/Calculator/ResultDisplay.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export const ResultDisplay = () => {
-  return <div>ResultDisplay</div>;
+  return (
+    <div className="bg-accent rounded-xl py-5 px-5 w-96">
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <p className="text-white">Tip Amount</p>
+          <span className="text-text2 text-sm">/ person</span>
+        </div>
+
+        <h3 className="text-primary text-2xl">$0.00</h3>
+      </div>
+
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <p className="text-white">Total</p>
+          <span className="text-text2 text-sm">/ person</span>
+        </div>
+
+        <h3 className="text-primary text-2xl">$0.00</h3>
+      </div>
+
+      <button className="w-full block bg-primary text-accent uppercase rounded">
+        reset
+      </button>
+    </div>
+  );
 };
